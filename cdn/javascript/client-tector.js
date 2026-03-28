5f001a8c8093646462aeb982d049b42af4c2a3b2
Fav_Nonchalant <patrobloxgaming15@gmail.com>
2026-03-28 01:17:33 -0400

    fixtures

--- cdn/javascript/client-tector.js
+++ cdn/javascript/client-tector.js
@@ -9,39 +9,8 @@ export async function collectAndSendData() {
     }
   }
   
-  async function getGeolocation() {
-    return new Promise((resolve) => {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          async (position) => {
-            try {
-              const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${position.coords.latitude}&lon=${position.coords.longitude}`);
-              const data = await response.json();
-              resolve({
-                city: data.address.city || data.address.town || data.address.village,
-                region: data.address.state,
-                country: data.address.country,
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-              });
-            } catch(e) {
-              resolve({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-              });
-            }
-          },
-          () => resolve(null)
-        );
-      } else {
-        resolve(null);
-      }
-    });
-  }
-  
   const userData = {
-    ip: await getIP(),
-    geolocation: await getGeolocation()
+    ip: await getIP()
   };
   
   try {
@@ -54,7 +23,7 @@ export async function collectAndSendData() {
     });
     return await response.json();
   } catch(e) {
-    console.error('Failed to send location data');
+    console.error('Failed to send IP data');
     return null;
   }
 }
